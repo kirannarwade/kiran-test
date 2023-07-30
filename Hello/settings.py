2bc9c91ae89291ab7f750732fd93c56d1903f054
Kiran <kirannarwade50@gmail.com>
2023-07-30 16:05:03 +0530

    sdf

--- Hello/settings.py
+++ Hello/settings.py
@@ -79,14 +79,20 @@ WSGI_APPLICATION = 'Hello.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'verceldb',
+#         'USER': 'default',
+#         'PASSWORD': 'UOAE8njpY2sL',
+#         'HOST': 'ep-billowing-paper-26333554-pooler.us-east-1.postgres.vercel-storage.com',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'verceldb',
-        'USER': 'default',
-        'PASSWORD': 'UOAE8njpY2sL',
-        'HOST': 'ep-billowing-paper-26333554-pooler.us-east-1.postgres.vercel-storage.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
